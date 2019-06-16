84727f3aff71cc6dc94afda12e99376771e24955
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2019-06-17 00:08:12 +0300

    test: Add integration tests for Fibonacci endpoint

--- src/Readify.KnockKnock.IntegrationTests/FibonacciTests.cs
+++ src/Readify.KnockKnock.IntegrationTests/FibonacciTests.cs
@@ -17,7 +17,27 @@ namespace Readify.KnockKnock.IntegrationTests
         }
 
         [Theory]
+        [InlineData(-92, -7540113804746346429)]
+        [InlineData(-47, 2971215073)]
+        [InlineData(-46, -1836311903)]
         [InlineData(-7, 13)]
+        [InlineData(-6, -8)]
+        [InlineData(-5, 5)]
+        [InlineData(-4, -3)]
+        [InlineData(-3, 2)]
+        [InlineData(-2, -1)]
+        [InlineData(-1, 1)]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(46, 1836311903)]
+        [InlineData(47, 2971215073)]
+        [InlineData(92, 7540113804746346429)]
         public async Task GivenGetFibonacci_WhenPassCorrectValuesOfN_ShouldReturnSuccessWithCorrectResult(long n, long expected)
         {
             var client = _server.CreateClient();
@@ -28,6 +48,22 @@ namespace Readify.KnockKnock.IntegrationTests
 
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(-93)]
+        [InlineData(93)]
+        [InlineData(-9223372036854775808)]
+        [InlineData(9223372036854775807)]
+        [InlineData(-2147483648)]
+        [InlineData(2147483647)]
+        public async Task GivenGetFibonacci_WhenPassTooBigValuesOfN_ShouldReturnBadRequest(long n)
+        {
+            var client = _server.CreateClient();
+
+            var response = await client.GetAsync($"{EndpointUrl}?n={n}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
